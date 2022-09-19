69d4eeedaad889e2c9bfa954961f1cde311c84fe
Serge Rozkov <rozkov_ss@mail.ru>
2022-09-19 22:39:49 +0300

    ДЗ.1 done

--- src/home_work1/Task1.java
+++ src/home_work1/Task1.java
@@ -1,4 +1,33 @@
 package home_work1;
 
+/**
+ * Задание 1
+ * <p>
+ * В переменной minutes лежит число от 0 до 59.
+ * Написать функцию, которая принимает в качестве параметра значение переменной minutes и выводит на консоль
+ * в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+ * <p>
+ * Протестировать функцию в main.
+ */
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(solution(59));
+    }
+
+    public static String solution(int minutes) {
+        if (minutes >= 0 && minutes < 15) {
+            return "Первая четверть часа";
+        } else if (minutes >= 15 && minutes < 30) {
+            return "Вторая четверть часа";
+        } else if (minutes >= 30 && minutes < 45) {
+            return "Третья четверть часа";
+        } else if (minutes >= 45 && minutes < 60) {
+            return "Четвертая четверть часа";
+        } else {
+            return "Неверные данные";
+        }
+    }
 }
+
+
--- src/home_work1/Task3.java
+++ src/home_work1/Task3.java
@@ -1,4 +1,30 @@
 package home_work1;
 
+/**
+ * Даны два прямоугольных треугольника.
+ * Каждый из них задается двумя целочисленными переменными a и b - стороны треугольника.
+ * Написать код, в котором вычисляется площадь каждого треугольника и затем эти площади сравниваются друг с другом.
+ */
+
 public class Task3 {
+    public static void main(String[] args) {
+        compareTriangle(11,3,5,6);
+    }
+
+    public static double getTriangleSquare(int a, int b) {
+        return (a * b) / 2;
+    }
+
+    public static void compareTriangle(int a1, int b1, int a2, int b2) {
+        double square1 = getTriangleSquare(a1, b1);
+        double square2 = getTriangleSquare(a2, b2);
+
+        if (square1 > square2) {
+            System.out.println("Треугольник 1 больше");
+        } else if (square1 < square2) {
+            System.out.println("Треугольник 2 больше");
+        } else {
+            System.out.println("Треугольники равны");
+        }
+    }
 }
