b89701476510e5606c473d92858c6cc50cfbf7b6
Serge Rozkov <rozkov_ss@mail.ru>
2022-09-19 22:39:49 +0300

    ДЗ.1 done

--- src/home_work1/Task2.java
+++ src/home_work1/Task2.java
@@ -1,4 +1,41 @@
 package home_work1;
 
+/**
+ * Даны 3 переменные:
+ * <p>
+ * - operand1 (double)
+ * - operand2 (double)
+ * - operation (char ‘+’, ‘-’, ‘*’, ‘/’, ‘%’)
+ * <p>
+ * Написать функцию, которая принимает в качестве параметров эти три переменные и возвращает результат операции.
+ * Протестировать функцию в main.
+ */
+
 public class Task2 {
+    public static void main(String[] args) {
+        solution(24.4, 10.1, '+');
+    }
+
+    public static void solution(double operand1, double operand2, char operation) {
+        switch (operation) {
+            case '+': {
+                System.out.println(operand1 + operand2);
+                break;
+            }
+            case '-': {
+                System.out.println(operand1 - operand2);
+                break;
+            }
+            case '*': {
+                System.out.println(operand1 * operand2);
+                break;
+            }
+            case '/': {
+                System.out.println(operand1 / operand2);
+                break;
+            }
+            default:
+                System.out.println("Неверные данные");
+        }
+    }
 }
