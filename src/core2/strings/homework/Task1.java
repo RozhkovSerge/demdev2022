9dfccc9189c3424e24ec2144b92d610eb2f6f2f5
Serge Rozkov <rozkov_ss@mail.ru>
2022-09-28 16:41:17 +0300

    Домашнее задание "Строки"

---
+++ src/core2/strings/homework/Task1.java
@@ -0,0 +1,30 @@
+package core2.strings.homework;
+
+/**
+ * Задание 1
+ * Дана строка.
+ * Удалить из нее все повторяющиеся символы без учета регистра, если они идут друг за другом. Также удалить пробелы.
+ * Результат привести к верхнему регистру.
+ * <p>
+ * Например:
+ * "abc Cpddd Dio OsfWw" -> "ABCPDIOSFW"
+ */
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicateSymbolsFromString("abc Cpddd Dio OsfWw!"));
+
+    }
+
+    private static String removeDuplicateSymbolsFromString(String str) {
+        StringBuilder sb = new StringBuilder();
+        String formattedString = str.replace(" ", "").toUpperCase() + " ";
+        for (int i = 0; i < formattedString.length()-1; i++) {
+            if (formattedString.charAt(i) != formattedString.charAt(i + 1)) {
+                sb.append(formattedString.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
